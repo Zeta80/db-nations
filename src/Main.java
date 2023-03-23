efef312a3b6b1722f23181d7e76205526d0d6321
Mirko Robbi <mirko.robbi@gmail.com>
2023-03-23 15:04:53 +0100

    ADD stamp query (milestone 2)

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+            String url = "jdbc:mysql://localhost/db_nations";
+            String user = "root";
+            String password = "root";
+
+            try (Connection conn = DriverManager.getConnection(url, user, password);
+                 Statement stmt = conn.createStatement();
+                 ResultSet rs = stmt.executeQuery("SELECT c.country_id, c.name AS country_name, r.name AS region_name, ct.name AS continent_name FROM countries c JOIN regions r ON c.region_id = r.region_id JOIN continents ct ON r.continent_id = ct.continent_id ORDER BY c.name")) {
+
+                while (rs.next()) {
+                    int countryId = rs.getInt("country_id");
+                    String countryName = rs.getString("country_name");
+                    String regionName = rs.getString("region_name");
+                    String continentName = rs.getString("continent_name");
+
+                    System.out.println(countryId + "\t" + countryName + "\t" + regionName + "\t" + continentName);
+                }
+            } catch (SQLException e) {
+                System.err.println("Error: " + e.getMessage());
+            }
+        }
+    }
