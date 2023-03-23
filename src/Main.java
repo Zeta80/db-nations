00e056567f3cc693b1cc6fdc6071863c47b2e601
Mirko Robbi <mirko.robbi@gmail.com>
2023-03-23 15:20:11 +0100

    ADD stamp query (milestone 3)

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,20 +8,32 @@ public class Main {
             String user = "root";
             String password = "root";
 
-            try (Connection conn = DriverManager.getConnection(url, user, password);
-                 Statement stmt = conn.createStatement();
-                 ResultSet rs = stmt.executeQuery("SELECT c.country_id, c.name AS country_name, r.name AS region_name, ct.name AS continent_name FROM countries c JOIN regions r ON c.region_id = r.region_id JOIN continents ct ON r.continent_id = ct.continent_id ORDER BY c.name")) {
+            // Chiediamo all'utente di inserire una stringa di ricerca
+        Scanner input = new Scanner(System.in);
+        System.out.print("Inserisci una stringa di ricerca: ");
+        String search = input.nextLine();
 
-                while (rs.next()) {
-                    int countryId = rs.getInt("country_id");
-                    String countryName = rs.getString("country_name");
-                    String regionName = rs.getString("region_name");
-                    String continentName = rs.getString("continent_name");
+        try (Connection conn = DriverManager.getConnection(url, user, password);
+             PreparedStatement stmt = conn.prepareStatement("SELECT c.country_id, c.name AS country_name, r.name AS region_name, ct.name AS continent_name FROM countries c JOIN regions r ON c.region_id = r.region_id JOIN continents ct ON r.continent_id = ct.continent_id WHERE c.name LIKE ? ORDER BY c.name")) {
 
-                    System.out.println(countryId + "\t" + countryName + "\t" + regionName + "\t" + continentName);
-                }
-            } catch (SQLException e) {
-                System.err.println("Error: " + e.getMessage());
+            // Aggiungiamo il parametro di ricerca alla query
+            stmt.setString(1, "%" + search + "%");
+
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                int countryId = rs.getInt("country_id");
+                String countryName = rs.getString("country_name");
+                String regionName = rs.getString("region_name");
+                String continentName = rs.getString("continent_name");
+
+                System.out.println(countryId + "\t" + countryName + "\t" + regionName + "\t" + continentName);
             }
+
+            rs.close();
+        } catch (SQLException e) {
+            System.err.println("Error: " + e.getMessage());
         }
     }
+        }
+
